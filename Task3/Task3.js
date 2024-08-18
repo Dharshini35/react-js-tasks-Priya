e6162645d2938cf188a346b16a2d7f51e1e23b7a
Dharshini35 <r.priyadharshini4874@gmail.com>
2024-08-18 19:51:12 +0530

    Task3

---
+++ Task3/Task3.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Img1 from "./Image/pc.jpg";
+function Task3() {
+    return(
+        <>
+         <div>
+            <h1>Social Buttons</h1>
+            <button type="button" className="btn btn-primary-m5">Like</button>
+            <button type="button" className="btn1 btn-success-m5">Command</button>
+            <button type="button" className="btn2 btn-warning-m5">Share</button>
+         </div>
+        </>
+                   
+    )
+}
+
+export default Task3;
